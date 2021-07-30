0b4725d0b89a22028a9ea0af76cdb067feaa5a7b
Sam Roble <smroble1996@gmail.com>
2021-07-30 13:50:07 -0600

    phase oen challenge attempt 2ish

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const fgComments = el('fg-comments');
 const likeButton = el('like-button');
 const commentForm = el('comment-form');
 
-let likes = 0;
+likes = 0;
 
-likeButton.addEventListener("click", () => {
-    likes += 1;
-    renderLikes();
-});
-
+likeButton.addEventListener('click', () => {
+        likes += 1;
+        renderLikes(likes)
+    });
+    
 commentForm.addEventListener("submit", (e) => {
     e.preventDefault();
     setComment(e.target.comment.value);
@@ -29,26 +29,30 @@ function renderImage(data){
     fgTitle.innerText = data.title;
     fgImage.src = data.image;
     likes = data.likes;
-    renderLikes();
-    setComment(data.comments);
-
+    renderLikes(likes);
+    loadcomment(data.comments)
 }
 
 function renderLikes(likes){
 fgLikes.innerText = `${likes} likes`
 };
 
-function setComment(comments) {
-    fgComments.innerHTML = '';
-    comments.forEach(comment => addComment(comment.content));
-}
-
 function setComment(comment){
     const li = document.createElement('li');
     li.innerText = comment;
+    console.log(comment)
     fgComments.append(li);
 }
 
 function el(id){
     return document.getElementById(id);
+}
+
+function loadcomment(comment){
+comment.forEach(comment => {
+        console.log(comment.content)
+        const li = document.createElement('li');
+    li.innerText = comment.content;
+    fgComments.append(li);
+    });
 }
